7e44f5c5fd560becea1758744cbdece963f16bea
derek <tillerderek@gmail.com>
2024-01-17 12:13:38 -0500

    v1

--- week2/react-poc-2-dtiller/src/index.js
+++ week2/react-poc-2-dtiller/src/index.js
@@ -2,23 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const lakeList = [
-  { id: "1", name: "Echo Lake", trailhead: "Echo Lake" },
-  { id: "2", name: "Brown Lake", trailhead: "Brown Lake" },
-  { id: "3", name: "Max Lake", trailhead: "Max Lake" },
+const mainNav = [
+  { linkText: 'Google', url: 'https://google.com' },
+  { linkText: 'Duck Duck Go', url: 'https://duckduckgo.com' },
+  { linkText: 'Ecosia', url: 'https://ecosia.com' },
 ];
 
-function App({ lakes }) {
+function PageHeader({ h1msg, pmsg }) {
   return (
-    <div>
-      {lakes.map(lake => (
-        <div>
-          <h2>{lake.name}</h2>
-          <p>Accessed by: {lake.trailhead}</p>
-        </div>
-      ))}
-    </div>
+    <header>
+      <h1>{h1msg}</h1>
+      <p>{pmsg}</p>
+    </header>
   );
 }
 
-ReactDOM.render(<App lakes={lakeList} />, document.getElementById('root'));
+function MainNavBar({ nav }) {
+  return (
+    <nav>
+      <ul>
+        {nav.map((itm) => (
+          <li>
+            <a href={itm.url}>{itm.linkText}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+function NormalContent() {
+  return <p>Sorry - no specials.</p>;
+}
+
+function PromoContent() {
+  return <p className="specials">We are currently running specials!</p>;
+}
+
+function MainContent({ promo }) {
+  return promo ? <PromoContent /> : <NormalContent />;
+}
+
+ReactDOM.render(
+  <>
+    <PageHeader h1msg="My Web Site" pmsg="We Do Good Things" />
+    <MainNavBar nav={mainNav} />
+    <MainContent promo={false} />
+  </>,
+  document.getElementById('root')
+);
